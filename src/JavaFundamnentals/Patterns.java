0f1167ac508403bd390ba1c8c629df0dc269e211
Zailta <mannanbhat60@gmail.com>
2021-09-12 15:23:05 +0530

    Pattern2 in java

--- src/JavaFundamnentals/Patterns.java
+++ src/JavaFundamnentals/Patterns.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class Patterns {
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
-        pattern1(n);
+       // Scanner scn = new Scanner(System.in);
+        int n = 5;
+        pattern2(n);
     }
     static void pattern1(int n){
         for (int i = 1; i <= n; i++)
@@ -15,6 +15,18 @@ public class Patterns {
             {
                 System.out.print("*	");
             }
+            System.out.println();
+        }
+    }
+
+    static void pattern2(int n){
+        for (int i = 1; i <= n ; i++)
+        {
+            for (int j = 1; j <= n -i+1; j++)
+            {
+                System.out.print("*	");
+            }
+
             System.out.println();
         }
     }
