aaf4b5e4469d0ec847a924796cd99dc6ab8d42a2
Zailta <mannanbhat60@gmail.com>
2021-09-12 16:04:24 +0530

    Pattern in java

--- src/JavaFundamnentals/Patterns.java
+++ src/JavaFundamnentals/Patterns.java
@@ -6,7 +6,7 @@ public class Patterns {
     public static void main(String[] args) {
        // Scanner scn = new Scanner(System.in);
         int n = 5;
-        pattern2(n);
+        pattern5(n);
     }
     static void pattern1(int n){
         for (int i = 1; i <= n; i++)
@@ -29,5 +29,51 @@ public class Patterns {
 
             System.out.println();
         }
+    }
+    static  void pattern3(int n){
+        for(int i = 1; i<=n;i++){
+            for(int j= 1; j<= n-i; j++){
+                System.out.print(" \t");
+            }
+            for(int k = 1; k<= i; k++){
+                System.out.print("*\t");
+            }
+            System.out.println();
+        }
+    }
+    static void pattern4(int n){
+        for(int i =1; i<= n; i++ ){
+            for(int j = 1; j<i; j++){
+                System.out.print("\t");
+            }for(int k = 1; k<=n-i+1; k++){
+                System.out.print("*\t");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern5(int n){
+        int spaces = n/2;
+        int stars  = 1;
+        for(int i = 1; i<=n ;i++){
+
+            for(int j = 1;j<=spaces;j++){
+                System.out.print("\t");
+            }
+            for(int k = 1; k<=stars; k++){
+                System.out.print("*\t");
+            }
+            if(i <= n/2){
+                spaces--;
+                stars+=2;
+            }
+            else {
+                spaces ++;
+                stars -=2;
+            }
+            System.out.println();
+
+        }
+
     }
 }
