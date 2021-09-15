4dfc9a885c30b0d58feea4c043a3c5361dbe4176
Zailta <mannanbhat60@gmail.com>
2021-09-15 22:59:16 +0530

    Pattern in java

--- src/JavaFundamnentals/Patterns.java
+++ src/JavaFundamnentals/Patterns.java
@@ -24,6 +24,17 @@ public class Patterns {
     }
 
     public static void pattern8(int n) {
+/* brute force approach :
+ for(int i = 1; i<=n;i++){
+            for(int j = 1; j<i; j++){
+                System.out.print("\t");
+            }
+            for(int k =1; k ==1; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+ */
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
