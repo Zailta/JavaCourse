2d77898357e7d2592ea8c6130b68c07dccba5f90
Zailta <mannanbhat60@gmail.com>
2021-09-17 07:57:16 +0530

    Pattern in java

--- src/JavaFundamnentals/Patterns.java
+++ src/JavaFundamnentals/Patterns.java
@@ -6,7 +6,8 @@ public class Patterns {
     public static void main(String[] args) {
        // Scanner scn = new Scanner(System.in);
         int n = 5;
-        pattern10(n);
+        pattern11(n);
+       /*
         pattern9(n);
         pattern8(n);
         pattern7(n);
@@ -16,6 +17,20 @@ public class Patterns {
         pattern3(n);
         pattern2(n);
         pattern1(n);
+
+        */
+
+    }
+
+    public static void pattern11(int n) {
+        int count  =1;
+        for(int i = 1; i<=n ;i++){
+            for(int j =1; j<i; j++){
+                System.out.print(count+"\t");
+                count++;
+            }
+            System.out.println();
+        }
 
     }
 /*
