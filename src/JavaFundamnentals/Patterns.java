7a127e74fa8771c43ff1c2eb46e0fef54dac76c8
Zailta <mannanbhat60@gmail.com>
2021-09-18 23:46:22 +0530

    Pattern in java

--- src/JavaFundamnentals/Patterns.java
+++ src/JavaFundamnentals/Patterns.java
@@ -23,12 +23,14 @@ public class Patterns {
     }
 
     private static void pattern13(int n) {
-        for(int i = 1; i<=n ;i++){
-            for(int j = 1; j<i;j++){
-                System.out.print("*\t");
-            }
-            for(int j = 1; j ==1 ; j++){
-                System.out.println(1+"\t");
+        for (int i = 0; i < n ; i++)
+        {
+            int icj = 1;
+            for (int j = 0; j <= i; j++)
+            {
+                System.out.print( icj + "	");
+                icj = (icj * (i - j)) / (j + 1);
+
             }
             System.out.println();
         }
