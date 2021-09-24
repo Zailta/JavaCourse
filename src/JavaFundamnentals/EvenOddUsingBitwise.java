3cbb251726f986e42e91fa6a508c8803164e533e
Zailta <mannanbhat60@gmail.com>
2021-09-24 20:13:06 +0530

    check even or odd using AND operator

--- src/JavaFundamnentals/EvenOddUsingBitwise.java
+++ src/JavaFundamnentals/EvenOddUsingBitwise.java
@@ -6,12 +6,21 @@ public class EvenOddUsingBitwise {
     public static void main(String[] args) {
         int n =1076;
         System.out.println(isodd(n));
-      //  EvenorOdd(n);
+        EvenorOdd(n);
     }
     public static boolean isodd(int n){
 
        // one way of checking odd and even
         return (n & 1) ==0;
     }
-
+    public static void EvenorOdd(int n ){
+       // for odd check
+        boolean check = (n&1) ==1;  // or you can try n&1 == 0 for even check.
+        if( check == true){
+            System.out.println("Number is odd");
+       }
+        else {
+            System.out.println("Number is even");
+        }
+    }
 }
