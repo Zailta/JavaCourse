5c0eb4d40956eeef625fadd9be72638d2c8226be
Zailta <mannanbhat60@gmail.com>
2021-09-27 07:47:47 +0530

    sieve of eratosthenes- primenumber optimisation (bitwise sieve)

--- src/SieveOfEratosthenes.java
+++ src/SieveOfEratosthenes.java
@@ -7,6 +7,7 @@ public class SieveOfEratosthenes {
         System.out.println(!prime[0]);
        Arrays.fill(prime, true);
         sieve(n, prime);
+        biwiseseive(n, prime);
     }
 
     private static void sieve(int n, boolean[] prime) {
@@ -18,6 +19,25 @@ public class SieveOfEratosthenes {
             }
         }
         for(int i = 2; i<=n;i++) {
+            if (prime[i]){
+                System.out.print(i+ " ");
+            }
+        }
+    }
+    private static void biwiseseive(int n, boolean[] prime) {
+        /*We know how to calculate all primes less than n by the Sieve of Eratosthenes. Below is an implementation of Sieve.
+One optimization in the below implementation is, we have skipped all even numbers altogether.
+We reduce the size of the prime array to half. We also reduce all iterations to half.
+         */
+        for(int i =3;i<Math.sqrt(n);i+=2){
+            if(prime[i]) {
+                for (int j = i * i; j <n; j += i) {
+                    prime[j] = false;
+                }
+            }
+        }
+        System.out.println();
+        for(int i = 3; i<n;i+=2) {
             if (prime[i]){
                 System.out.print(i+ " ");
             }
