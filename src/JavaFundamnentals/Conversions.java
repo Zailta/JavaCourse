31322651d53183660f358535e91f1499526b04d9
Zailta <mannanbhat60@gmail.com>
2021-09-27 11:05:26 +0530

    AnyBaseToDecimal

--- src/JavaFundamnentals/Conversions.java
+++ src/JavaFundamnentals/Conversions.java
@@ -5,6 +5,8 @@ public class Conversions {
         int n = 100;
         int base = 2;
         DecimalToAnyBase(n,base);
+        int i = AnyBaseToDecimal(n, 2);
+        System.out.println(i);
 
     }
     public static void DecimalToAnyBase(int n, int b){
@@ -17,8 +19,21 @@ public class Conversions {
             sum += (temp*p);
             p*=10;
 
+
         }
         System.out.println(sum);
+    }
 
+    public static int AnyBaseToDecimal(int n, int b){
+        // write your code here
+        int p  =0;
+        int sum = 0;
+        while (n>0){
+            int digit = n%10;
+            sum += digit*((int) Math.pow(b,p));
+            n = n/10;
+            p++;
+        }
+        return sum;
     }
 }
