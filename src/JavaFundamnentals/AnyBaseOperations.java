feb92ceef791a0e2c9ee4bdb98793bda3a8073aa
Zailta <mannanbhat60@gmail.com>
2021-10-06 22:05:11 +0530

    Any BAse Operations

---
+++ src/JavaFundamnentals/AnyBaseOperations.java
@@ -0,0 +1,60 @@
+package JavaFundamnentals;
+
+public class AnyBaseOperations {
+    public static void main(String[] args) {
+       // AnyBaseAddition(8, 236, 754);
+       // AnyBaseSubtraction(8,256,1212);
+       // AnyBaseMultiplication(5, 1220, 31);
+    }
+
+
+
+    private static void AnyBaseSubtraction(int base , int number1, int number2) {
+        int carry  =0;
+        int power = 1;
+        int returnvalue = 0;
+        while(number2>0  ) {
+            int rem1 = number1 % 10;
+            int rem2 = number2 % 10;
+            number1 /= 10;
+            number2 /= 10;
+            //assuming number 2 is always bigger than number 1:
+            int ans =0;
+            rem2 = rem2+carry;
+            if(rem2>=rem1){
+                carry = 0;
+                ans = rem2=rem1;
+            }
+            else {
+                carry = -1;
+                ans = rem2+base-rem1;
+            }
+            returnvalue += ans*power;
+            power*=10;
+        }
+        System.out.println(returnvalue);
+
+    }
+
+    private static int AnyBaseAddition(int base, int number1, int number2) {
+        int carry  =0;
+        int power = 1;
+        int returnvalue = 0;
+
+        while(number1>0|| number2>0 || carry>0 ){
+            int rem1 = number1%10;
+            int rem2 = number2%10;
+            number1/=10;
+            number2/=10;
+            int sum = rem1+rem2+carry;
+            carry = sum/base;
+            int ans= sum%base;
+            returnvalue += ans*power;
+            power*=10;
+
+
+        }
+        System.out.println(returnvalue);
+        return returnvalue;
+    }
+}
