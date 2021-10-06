93a927f22c51fddcbe88ff61e607a86813505b82
Zailta <mannanbhat60@gmail.com>
2021-10-06 22:07:38 +0530

    Any Base Operations

--- src/JavaFundamnentals/AnyBaseOperations.java
+++ src/JavaFundamnentals/AnyBaseOperations.java
@@ -4,10 +4,30 @@ public class AnyBaseOperations {
     public static void main(String[] args) {
        // AnyBaseAddition(8, 236, 754);
        // AnyBaseSubtraction(8,256,1212);
-       // AnyBaseMultiplication(5, 1220, 31);
+       AnyBaseMultiplication(5, 1220, 31);
     }
 
 
+    public static int SingledigitMultiplication(int base, int number1, int digit ){
+        int carry =0;
+        int rev = 0;
+        while(number1>0 || carry>0){
+            System.out.println("Hello");
+        }
+        return rev;
+    }
+    private static void AnyBaseMultiplication(int base, int number1, int number2) {
+        int answer = 0;
+        int power =1;
+        while(number2!=0){
+            int multiplier = number2%10;
+            number2/=10;
+            answer += (number1*multiplier*power);
+            power*=10;
+        }
+        System.out.println(answer);
+
+    }
 
     private static void AnyBaseSubtraction(int base , int number1, int number2) {
         int carry  =0;
