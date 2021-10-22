cd1b211be6aaaef354ba62ad63c9ddd8c8ab6a04
Zailta <mannanbhat60@gmail.com>
2021-10-22 12:19:25 +0530

    Wave iteration in 2 d array

--- src/JavaFundamnentals/TwoDimentionalArray.java
+++ src/JavaFundamnentals/TwoDimentionalArray.java
@@ -3,7 +3,30 @@ package JavaFundamnentals;
 public class TwoDimentionalArray {
     public static void main(String[] args) {
         int[][] arr = {{1,2,3},{4,5,6},{7,8,9,}};
-        multiplication(arr, arr);
+       // multiplication(arr, arr);
+        waveIteration(arr, 3,3);
+    }
+
+    private static void waveIteration(int[][]arr, int rows, int coloumns) {
+        for(int coloumn  = 0; coloumn<coloumns; coloumn++){
+            if(coloumn%2 ==0){
+                /* for the wave pattern we assume that the number of rows is decreasing
+                when we go down the column (even numbers of column)  and it is decreases when we go up the column(odd number of column):
+
+
+                 */
+                for(int row  = 0; row<rows; row++){
+                    System.out.print(arr[row][coloumn]);
+                }
+            }
+            else {
+                for(int row = rows-1; row>=0;row--){
+                    System.out.print(arr[row][coloumn]);
+                }
+
+            }
+            System.out.println();
+        }
     }
 
     private static void multiplication(int[][] arr, int[][] arr2) {
