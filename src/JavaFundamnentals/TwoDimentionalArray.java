a87c7ecd479e322ee3853590711632a22ef038cb
Zailta <mannanbhat60@gmail.com>
2021-10-22 15:36:01 +0530

    spiral iteration in 2d array

--- src/JavaFundamnentals/TwoDimentionalArray.java
+++ src/JavaFundamnentals/TwoDimentionalArray.java
@@ -2,9 +2,52 @@ package JavaFundamnentals;
 
 public class TwoDimentionalArray {
     public static void main(String[] args) {
-        int[][] arr = {{1,2,3},{4,5,6},{7,8,9,}};
+        int[][] arr = {{1,2,3},{4,5,6},{7,8,9}};
        // multiplication(arr, arr);
-        waveIteration(arr, 3,3);
+      //  waveIteration(arr, 3,3);
+        spiraliteration(arr);
+    }
+
+    private static void spiraliteration(int [][] arr) {
+        int rows = arr.length;
+        int coloumns = arr[0].length;
+
+        int elements = rows*coloumns;
+        int count =0;
+        int minr = 0;
+        int minc = 0;
+        int maxr = arr.length-1;
+        int maxc = arr[0].length-1;
+        while (count < elements){
+            //left
+            for(int row = minr; row<= maxr && count < elements ; row++ ){
+                System.out.print(arr[row][minc]);
+                count++;
+            }
+            minc++;
+
+
+            //bottom
+            for(int coloumn = minc; coloumn<= maxc  && count < elements; coloumn++ ){
+                System.out.print(arr[maxr][coloumn]);
+                count++;
+            }
+            maxr--;
+
+            //right
+            for(int row = maxr; row>= minr  && count < elements; row-- ){
+                System.out.print(arr[row][maxc]);
+                count++;
+            }
+            maxc--;
+            //upper
+            for(int coloumn = maxc; coloumn>=minc  && count < elements ; coloumn-- ){
+                System.out.print(arr[minr][coloumn]);
+                count++;
+            }
+            minr++;
+
+        }
     }
 
     private static void waveIteration(int[][]arr, int rows, int coloumns) {
