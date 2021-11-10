3e0f902fa1f91c0889739028126270fac42a5146
Zailta <mannanbhat60@gmail.com>
2021-11-10 15:17:40 +0530

    Ring rotate

--- src/JavaFundamnentals/TwoDimentionalArray.java
+++ src/JavaFundamnentals/TwoDimentionalArray.java
@@ -10,9 +10,124 @@ public class TwoDimentionalArray {
        // exitarray(bin);
       //  roateanarrayby90degree(arr);
      //   digonaltransversal(arr);
+        rotateShell(arr, 1,1);
         //saddlepoint(arr);
-        seracharray(arr, 3);
+       // seracharray(arr, 3);
+        display1(arr);
     }
+
+    public static void rotateShell(int [][]arr, int s, int r){
+        int[] oned = shelltoOned(arr, s);
+        rotate(oned, r);
+        onedtoshell(arr, s, oned);
+
+    }
+
+    // Shell to oned:
+
+    public static int[] shelltoOned(int[][]arr, int s){
+        int minr = s-1;
+        int minc = s-1;
+        int maxr = arr.length-s;
+        int maxc = arr[0].length-s;
+        int size = 2*(maxr-minr + maxc - minc );
+        int [] oned = new int[size];
+        int idx = 0;
+
+        //left
+        for(int i = minr, j = minc; i<=maxr; i++){
+            oned[idx] = arr[i][j];
+            idx++;
+        }
+
+
+        //bottom
+
+        for(int i = maxr, j = minc+1; j<=maxc; j++){
+            oned[idx] = arr[i][j];
+            idx++;
+        }
+        //right
+        for(int i = maxr-1, j = maxc; i>=minr ; i--){
+            oned[idx] = arr[i][j];
+            idx++;
+
+        }
+        //up
+        for(int i = minr, j = maxc-1; j>= minc+1; j--){
+            oned[idx] = arr[i][j];
+            idx++;
+        }
+
+        return oned;
+    }
+
+    //oned to shell :
+
+
+    public static void onedtoshell(int[][]arr, int s, int[] oned){
+        int minr = s-1;
+        int minc = s-1;
+        int maxr = arr.length-s;
+        int maxc = arr[0].length-s;
+        int idx = 0;
+
+        //left
+        for(int i = minr, j = minc; i<=maxr; i++){
+            arr[i][j] = oned[idx];
+            idx++;
+        }
+
+
+        //bottom
+
+        for(int i = maxr, j = minc+1; j<=maxc; j++){
+            arr[i][j] = oned[idx];
+            idx++;
+        }
+        //right
+        for(int i = maxr-1, j = maxc; i>=minr ; i--){
+            arr[i][j] = oned[idx];
+            idx++;
+
+        }
+        //up
+        for(int i = minr, j = maxc-1; j>= minc+1; j--){
+            arr[i][j] = oned[idx];
+            idx++;
+        }
+
+    }
+    public static void rotate(int[] oned, int r) {
+        r = r % oned.length;
+        if (r < 0) {
+            r += oned.length;
+        }
+
+        reverse(oned, 0, oned.length - r - 1);
+        reverse(oned, oned.length - r, oned.length - 1);
+        reverse(oned, 0, oned.length - 1);
+    }
+
+    public static void reverse(int[] arr, int i, int j) {
+        while (i < j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
+    public static void display1(int[][] arr){
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[0].length; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
 
     private static void seracharray(int[][] arr, int data) {
         int i = 0; int j = arr[0].length-1;
