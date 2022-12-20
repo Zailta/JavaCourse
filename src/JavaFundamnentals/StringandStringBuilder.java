7b2a6895fcf39307c03a463dc025e725ac446ab9
Zailta <mannanbhat60@gmail.com>
2022-12-20 21:57:56 +0530

    Strings

--- src/JavaFundamnentals/StringandStringBuilder.java
+++ src/JavaFundamnentals/StringandStringBuilder.java
@@ -1,17 +1,26 @@
 package JavaFundamnentals;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class StringandStringBuilder {
 
     public static void main(String[] args) {
-        String str = "mOHAMMAD mANNAN bHAT";
+       // String str = "mOHAMMAD mANNAN bHAT";
      //   solution(str);
       //  compressString(str);
       //  compressStringSet(str);
        // compressStringAlphabetiteration(str);
-        casechange(str);
+       // casechange(str);
+        int shifa  = 10;
+        String name = "Shifa";
+        Float number = 100.3f;
+        System.out.println(shifa);
+        System.out.println(name);
+        System.out.println(number);
+
 
     }
 
@@ -105,5 +114,63 @@ public class StringandStringBuilder {
            }}
 
    }
+   public static String checksum(String accountnumber, String bankcode) {
+
+        if(accountnumber==null|| bankcode == null){
+            return "";
+        }
+       List<Integer> weightage = new ArrayList<>();
+       List<Integer> bankcodelist = new ArrayList<>();
+       List<Integer> values = new ArrayList<>();
+       Integer checksum = 0;
+       String weight = "7315248616";
+       for (int i = 0; i < weight.length(); i++) {
+           char ch = weight.charAt(i);
+           int number = Integer.parseInt(String.valueOf(ch));
+           weightage.add(number);
+       }
+       for (int i = 0; i < accountnumber.length(); i++) {
+           char ch = accountnumber.charAt(i);
+           int number = Integer.parseInt(String.valueOf(ch));
+           values.add(number);
+       }
+       for (char ch : bankcode.toCharArray()) {
+           if (Character.isLetter(ch)) {
+               int pos = ch - 'a' + 10;
+               bankcodelist.add(pos);
+           } else {
+               bankcodelist.add(Integer.parseInt(String.valueOf(ch)));
+           }
+
+       }
+       int length = values.size() + bankcodelist.size();
+
+
+       for (int i = 0; i < length; i++) {
+
+
+           if (length >= 7) {
+                checksum += bankcodelist.get(i) * weightage.get(i);
+           } else {
+                checksum += values.get(i) * weightage.get(i);
+
+           }
+       }
+       int finalresult = 0;
+       if(checksum%2 == 0){
+           finalresult =  checksum %89;
+       }
+       else {
+           finalresult = (checksum %89) - 89;
+
+       }
+      String check_sum = String.valueOf(finalresult);
+       String finalaccountNumber = check_sum+"-"+accountnumber;
+
+
+       return finalaccountNumber;
+
+   }
+
 
 }
---
+++ src/com/java/OOP/basics/OOPFundamentals.java
@@ -0,0 +1,7 @@
+package com.java.OOP.basics;
+
+public class OOPFundamentals {
+    public static void main(String[] args) {
+
+    }
+}
