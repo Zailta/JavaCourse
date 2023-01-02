f995fbec41ff809c9da2d8c2f42a899109ca9c30
Zailta <mannanbhat60@gmail.com>
2023-01-02 20:06:11 +0530

    Stream API Map Example

--- src/JavaFundamnentals/LambdaExpressions.java
+++ src/JavaFundamnentals/LambdaExpressions.java
@@ -5,7 +5,10 @@ import com.java.oop.purchaseprogram.Customer;
 
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class LambdaExpressions {
@@ -43,6 +46,27 @@ public class LambdaExpressions {
         };
         Thread mapRun = new Thread(mapThread);
         mapRun.run();
+
+        List <Integer> list1 = List.of(1,2,3,4,5,6,7,8);
+        List <Integer> list2 =List.of(5,6,7);
+        List <Integer> list3 = List.of(8,9,12);
+        List <String> list4 = List.of("Alpha","Beta","Gamma");
+
+        List<List<?>> mergedlist = Arrays.asList(list1,list2,list3, list4);
+        /***ways to call Stream api on lists
+         * using Functional Class
+         * Directly calling the map on stream
+         */
+        //first way:
+        System.out.println("First Way to use Map on Stream");
+        Function<List<?>, Integer> map = List::size;
+        mergedlist.stream().map(map).forEach(System.out::println);
+        System.out.println("Second way of using map on Stream");
+        //Second Way:
+        mergedlist.stream().map(List::size).forEach(System.out::println);
+        //use of flat map: a flat map returns a flatted map, meaning the data present in eac stream return as a single stream->
+        System.out.println("This is a flattened Map:");
+        mergedlist.stream().flatMap(Collection::stream).forEach(System.out::println);
 
 
 
