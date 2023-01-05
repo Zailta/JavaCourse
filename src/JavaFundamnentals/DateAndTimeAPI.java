65a02e2a694d33ae877042f30f4dccb5815ddcbb
Zailta <mannanbhat60@gmail.com>
2023-01-05 17:08:22 +0530

    Date and Time API in java 8

--- src/JavaFundamnentals/DateAndTimeAPI.java
+++ src/JavaFundamnentals/DateAndTimeAPI.java
@@ -1,20 +1,19 @@
 package JavaFundamnentals;
 
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.Month;
-import java.time.Period;
+import java.time.*;
+import java.time.temporal.ChronoUnit;
 
 public class DateAndTimeAPI {
     public static void main(String[] args) {
         Instant instant = Instant.now();
         LocalDate now = LocalDate.now();
         Period p = LocalDate.of(1947, Month.SEPTEMBER, 23).until(now);
-        p.getYears();
+         long until = LocalDate.of(1947, Month.SEPTEMBER, 23).until(now, ChronoUnit.DAYS);
         System.out.println("Instance is: "+instant);
         System.out.println("Local date is: "+now);
         System.out.println("Period is: "+p);
         System.out.println("Years Difference is: "+p.getYears());
+        System.out.println("To get Number of months/days/years from a particular date we use Chrono Unit: "+ until);
     }
 
 }
